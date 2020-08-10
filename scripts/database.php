66e2c986005ab45c6fcb2f8092b9b944bb687a6e
Callum <pilton.callum@gmail.com>
2020-08-10 21:02:21 +0100

    Add DB

--- scripts/database.php
+++ scripts/database.php
@@ -3,10 +3,11 @@
 
 //error_reporting(0);
 
-$host = "127.0.0.1"; // MySQL host name eg. localhost
-$user = "root"; // MySQL user. eg. root ( if your on localserver)
-$password = ""; // MySQL user password  (if password is not set for your root user then keep it empty )
-$database = "flashquiz"; // MySQL Database name
+$host = $_ENV["SQL_HOST"]; // MySQL host name eg. shareddb-p.hosting.stackcp.net
+$user = $_ENV["SQL_USER"]; // MySQL user. eg. root ( if your on localserver)
+$password = $_ENV["SQL_PASSWORD"]; // MySQL user password  (if password is not set for your root user then keep it
+// empty )
+$database = $_ENV["SQL_DATABASE"]; // MySQL Database name
 
 $DBH = new PDO("mysql:host=$host;dbname=$database", $user, $password);
  
